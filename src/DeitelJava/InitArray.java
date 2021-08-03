d244daa7f082eb5b4f620643d8aec34cc9abc785
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-08-03 10:02:35 -0300

    Cap 7/ Array e ArrayLists/ Fig 7.4 Calculando os valores a serem colocados nos elementos de um array!

--- src/DeitelJava/InitArray.java
+++ src/DeitelJava/InitArray.java
@@ -1,18 +1,23 @@
 package DeitelJava;
-//Figura 7.3: InitArray.java
-//Inicializando os elementos de um array com um inicializador de array
+//Figura 7.4: InitArray.java
+//Calculando os valores a serem colocados nos elementos de um array
 public class InitArray {
 
 	public static void main(String[] args) {
-		//A lista de inicializador especifica o valor inicial de cada elemento
-		int[] array = { 37, 45, 22, 85, 75, 15, 29, 55, 92, 33};
+		
+		final int ARRAY_LENGTH = 10;//declara constante
+		int[] array = new int[ARRAY_LENGTH];//cria o array
+		
+		//calcula o valor de cada elemento do array
+		for(int counter = 0; counter < array.length; counter++)
+			array[counter] = 2 + 2 * counter;
 		
 		System.out.printf("%s%8s%n", "Index", "Value");//titulos da coluna
 		
-		//gera saída do valor de cada elemento do array
+		//gera a saída do valor de cada elemento do array
 		for(int counter = 0; counter < array.length; counter++)
 			System.out.printf("%5d%8d%n", counter, array[counter]);
-	
+		
 	}
 
 }//fim da classe InitArray
