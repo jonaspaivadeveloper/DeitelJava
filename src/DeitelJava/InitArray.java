6fb263adf3f0ad28a4ee3be5a9a2b8013a145e26
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-08-03 09:30:25 -0300

    Cap 7/ Array e Arraylists/ Fig 7.3 Inicializando os elementos de um array com um inicializador de array!

--- src/DeitelJava/InitArray.java
+++ src/DeitelJava/InitArray.java
@@ -1,18 +1,18 @@
 package DeitelJava;
-//Figura 7.2: InitArray.java
-//Inicializando os elementos de um array como valores padrão de zero
+//Figura 7.3: InitArray.java
+//Inicializando os elementos de um array com um inicializador de array
 public class InitArray {
 
 	public static void main(String[] args) {
-		//declara array variável e o inicializa com um objeto array
-		int[] array = new int[10];//cria o objeto array
+		//A lista de inicializador especifica o valor inicial de cada elemento
+		int[] array = { 37, 45, 22, 85, 75, 15, 29, 55, 92, 33};
 		
-		System.out.printf("%s%8s%n", "Index", "Value");//titulos de coluna
+		System.out.printf("%s%8s%n", "Index", "Value");//titulos da coluna
 		
 		//gera saída do valor de cada elemento do array
 		for(int counter = 0; counter < array.length; counter++)
 			System.out.printf("%5d%8d%n", counter, array[counter]);
-
+	
 	}
 
-}
+}//fim da classe InitArray
